036110192884e983606ff718155bc914117ceddf
Muhammed Althaf <95875745+MagnusxAlt@users.noreply.github.com>
2024-09-27 18:10:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+
+    if (password !== confirmPassword) {
+        alert('Passwords do not match!');
+        event.preventDefault();
+    }
+});
